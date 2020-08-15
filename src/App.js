6348124cd931019fe1aff777cd5e1c8865b8afe3
Bignichok <bignik545@gmail.com>
2020-08-15 10:25:52 +0300

    final

--- src/App.js
+++ src/App.js
@@ -2,37 +2,98 @@ import React, { Component } from "react";
 import Total from "./components/total/Total";
 import History from "./components/history/History";
 import Operation from "./components/operation/Operation";
+import HistoryItem from "./components/history/HistoryItem";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      transactions: [],
+      transactions: JSON.parse(localStorage.getItem("calcMoney")) || [],
       description: "",
       amount: "",
+      resultIncome: 0,
+      resultExpenses: 0,
+      totalBalance: 0,
     };
   }
 
+  componentWillMount() {
+    this.getTotalBalance();
+  }
+
+  componentWillUpdate() {
+    this.addStorage();
+  }
   addTransaction = (add) => {
-    const transactions = [...this.state.transactions];
+    const transactions = [
+      ...this.state.transactions,
+      {
+        id: `cmr${(+new Date()).toString(16)}id`,
+        description: this.state.description,
+        amount: parseFloat(this.state.amount),
+        add,
+      },
+    ];
 
-    const transaction = {
-      id: `cmr${(+new Date()).toString(16)}id`,
-      description: this.state.description,
-      amount: this.state.amount,
-      add,
-    };
-
-    transactions.push(transaction);
-    this.setState({ transactions, description: "", amount: "" });
+    this.setState(
+      {
+        transactions,
+        description: "",
+        amount: "",
+      },
+      this.getTotalBalance
+    );
   };
 
-  addDescription = (e) => {
-    this.setState({ description: e.target.value });
-  };
+  addDescription = (e) => this.setState({ description: e.target.value });
 
-  addAmount = (e) => {
-    this.setState({ amount: e.target.value });
+  addAmount = (e) => this.setState({ amount: e.target.value });
+
+  getIncome = () =>
+    this.state.transactions.reduce(
+      (acc, item) => (item.add ? item.amount + acc : acc),
+      0
+    );
+  getExpenses = () =>
+    this.state.transactions.reduce(
+      (acc, item) => (!item.add ? item.amount + acc : acc),
+      0
+    );
+
+  // getIncome() {
+  //   return this.state.transactions
+  //     .filter((item) => item.add)
+  //     .reduce((acc, item) => item.amount + acc, 0);
+  // }
+
+  // getExpenses() {
+  //   return this.state.transactions
+  //     .filter((item) => !item.add)
+  //     .reduce((acc, item) => item.amount + acc, 0);
+  // }
+
+  getTotalBalance() {
+    const resultIncome = this.getIncome();
+    const resultExpenses = this.getExpenses();
+
+    const totalBalance = resultIncome - resultExpenses;
+
+    this.setState({
+      resultIncome,
+      resultExpenses,
+      totalBalance,
+    });
+  }
+
+  addStorage() {
+    localStorage.setItem("calcMoney", JSON.stringify(this.state.transactions));
+  }
+
+  deleteHistoryItem = (id) => {
+    const transactions = this.state.transactions.filter(
+      (item) => item.id !== id
+    );
+    this.setState({ transactions }, this.getTotalBalance);
   };
 
   render() {
@@ -45,8 +106,15 @@ class App extends Component {
 
         <main>
           <div className="container">
-            <Total />
-            <History transactions={this.state.transactions} />
+            <Total
+              resultExpenses={this.state.resultExpenses}
+              resultIncome={this.state.resultIncome}
+              totalBalance={this.state.totalBalance}
+            />
+            <History
+              transactions={this.state.transactions}
+              deleteHistoryItem={this.deleteHistoryItem}
+            />
             <Operation
               addTransaction={this.addTransaction}
               addDescription={this.addDescription}
--- src/components/history/History.js
+++ src/components/history/History.js
@@ -1,13 +1,17 @@
 import React from "react";
 import HistoryItem from "./HistoryItem";
 
-const History = ({ transactions }) => (
+const History = ({ transactions, deleteHistoryItem }) => (
   <>
     <section className="history">
       <h3>История расходов</h3>
       <ul className="history__list">
         {transactions.map((item) => (
-          <HistoryItem key={item.id} transaction={item} />
+          <HistoryItem
+            key={item.id}
+            transaction={item}
+            deleteHistoryItem={deleteHistoryItem}
+          />
         ))}
       </ul>
     </section>
